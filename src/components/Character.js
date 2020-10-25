12ea53b43822b6050d413b21d6daad57c8efb943
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2020-10-25 16:54:06 -0700

    MVP Completed

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,40 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components"
+
+const Character = props => {
+const { characterData } = props;
+
+const CharacterCard = styled.div`
+    width: 50%;
+    font-size: 1.5rem;
+	background-color: green ;
+	margin: 0 auto;
+	margin-bottom:2%;
+	color: lightgray;
+	padding: 0.2%;
+	border: 3px solid black;
+`
+const CharacterName = styled.h2`
+color: orange;
+`
+const CharacterInfo = styled.p`
+text-align: center;
+color: black
+` 
+
+console.log(characterData)
+	return (
+		<CharacterCard>
+			<CharacterName>
+				{characterData.name}
+			</CharacterName>
+			<CharacterInfo>{`height: ${characterData.height}`}</CharacterInfo>
+			<CharacterInfo>{`mass: ${characterData.mass}`}</CharacterInfo>
+			<CharacterInfo>{`gender: ${characterData.gender}`}</CharacterInfo>
+			<CharacterInfo>{`birth year: ${characterData.birth_year}`}</CharacterInfo>
+		</CharacterCard>
+	)
+
+}
+
+export default Character;
